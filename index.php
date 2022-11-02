9ca5d46f3802f8aa062a1ce5e1681b3b394a8ba8
Raj Goyanka <75495749+raj-goyanka@users.noreply.github.com>
2022-11-02 22:20:37 +0530

    Update index.php
    Updated index.php file

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 </head>
 <body>
         <h1><?php echo 'Hello, Mr. Raj Goyanka!'; ?></h1>
-     <h1><?php echo 'I am a student of SGSITS CSE !'; ?></h1>
+     <h1><?php echo 'I am a student of SGSITS CSE Indore Madhya Pradesh'></h1>
+   
 </body>
 </html>
